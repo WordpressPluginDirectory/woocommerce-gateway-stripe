d0be3b11803c5c9b21514b7b238cfd6590302c37
Justin Gardner <justin@rhynorater.com>
2025-12-10 06:02:52 -0500

    10.2.0

--- woocommerce-gateway-stripe/build/index.asset.php
+++ woocommerce-gateway-stripe/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '44dbda9e6ebaf90f6216');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3c13411fee1a7ecec730');
