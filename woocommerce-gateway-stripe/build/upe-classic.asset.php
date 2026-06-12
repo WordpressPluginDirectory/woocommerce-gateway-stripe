3e35a8d6a7be57fbf4859e1df87d84b2c6b86193
Justin Gardner <justin@rhynorater.com>
2026-06-12 06:01:34 -0500

    10.8.1

--- woocommerce-gateway-stripe/build/upe-classic.asset.php
+++ woocommerce-gateway-stripe/build/upe-classic.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '0c79654daf8e1e19720b');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wc-settings', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'd0323e09bf8ad70a1d59');
---
+++ woocommerce-gateway-stripe/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'woocommerce/woocommerce-gateway-stripe',
+        'pretty_version' => 'dev-release/10.8.1',
+        'version' => 'dev-release/10.8.1',
+        'reference' => 'a9aa74708541338f0cfabd5887a2b02cf512b958',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'woocommerce/woocommerce-gateway-stripe' => array(
+            'pretty_version' => 'dev-release/10.8.1',
+            'version' => 'dev-release/10.8.1',
+            'reference' => 'a9aa74708541338f0cfabd5887a2b02cf512b958',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
